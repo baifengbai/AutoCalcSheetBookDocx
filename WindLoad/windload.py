05bd3d7f5a3db0dc77107cd5e3973f0fee9dd437
WilliamTsuiChina <342668998@qq.com>
2020-06-10 21:38:03 +0800

    Update windload.py

--- WindLoad/windload.py
+++ WindLoad/windload.py
@@ -152,15 +152,17 @@ calc_book.core_properties.modified = datetime.utcnow()
 '''
 # 计算书名称
 jobname = '大疆ZSL1150抗风计算书'
-
+# 塔机型号
+tower_model = 'ZSL1150'
 # 非工作10m高处基本风压
-pn = 1000
+# pn = 1000
 # 塔机最高处的计算高度
-ht = 220
+# ht = 220
 # 风压高度变化系数, 保留两位小数
-kh = round(((((ht / 10) ** 0.14) + 0.4) / 1.4) ** 2, 2)
+# kh = round(((((ht / 10) ** 0.14) + 0.4) / 1.4) ** 2, 2)
 
 # 抗台风计算风压指定值
+# 深圳防台风技术规程5.1.3中最大风压1870Pa
 pnh = 1870
 # 吊臂长度
 beam_len = 58.5
@@ -181,6 +183,16 @@ hook_m = ceil(hook_mass * beam_len * cos(radians(beam_ang)))
 # 非工作状态下，上部结构不平衡
 m0 = tower_back + beam_m + hook_m
 
+# 吊臂
+beam_d1 = 133  # 主弦直径mm
+beam_d2 = 70  # 腹杆直径mm
+beam_b = 2.25  # 主弦中心距m
+beam_fg_len = 1.27 * beam_len * 2  # 腹杆长度总和
+beam_a = beam_len * 2 * beam_d1 * 0.001 + beam_fg_len * beam_d2 * 0.001  # 特征面积
+beam_phi = beam_a / (beam_len * (beam_b + beam_d1 * 0.001) * sin(radians(beam_ang)))   # 充实率
+wind_v = (pnh / 0.625)**0.5   # 通过风压反推风速
+re = 0.667 * wind_v * beam_d1 * 0.001  # 单位10^5
+
 '''
 文档生成
 '''
@@ -193,28 +205,54 @@ calc_book.add_paragraph(f'钩头产生的前倾力矩：{hook_m}t.m', style='Nor
 calc_book.add_paragraph(f'非工作状态下塔机上部结构总不平衡力矩：M={tower_back}t.m+{beam_m}t.m+{hook_m}t.m={m0}t.m', style='Normal')
 calc_book.add_paragraph('*为便于计算，规定由回转中心朝吊臂方向弯矩为正，由回转中心朝配重块方向弯矩为负。', style='Quote')
 
-calc_book.add_heading('2.1 非工作状态风载荷', level=2)
+calc_book.add_heading('2.2 非工作状态风载荷', level=2)
 mathtemp = r'F_{WN} = p_n(h) \times C \times A'
 width = add_image(mathtemp, 'fwn')
 calc_book.add_paragraph('', style='No Spacing').add_run('').add_picture(f'{path}/fwn.png', width=Inches(width))
 calc_book.add_paragraph('式中：', style='Normal')
+
 para1 = calc_book.add_paragraph('', style='Normal')
 mathtemp = r'F_{WN}'
-width = add_image(mathtemp, 'fwn1')
+add_image(mathtemp, 'fwn1')
 para1.add_run('').add_picture(f'{path}/fwn1.png', height=Mm(font_height))  # Inches(width))
 para1.add_run('——非工作状态垂直作用在所指构件纵轴线上的风载荷，单位为牛顿')
 mathtemp = r'(N)'
-width = add_image(mathtemp, 'N')
+add_image(mathtemp, 'N')
 para1.add_run('').add_picture(f'{path}/N.png', height=Mm(font_height))  # Inches(width))
 
 para1 = calc_book.add_paragraph('', style='Normal')
 mathtemp = r'p_n(h)'
-width = add_image(mathtemp, 'pnh')
+add_image(mathtemp, 'pnh')
 para1.add_run('').add_picture(f'{path}/pnh.png', height=Mm(font_height))  # Inches(width))
 para1.add_run('——高度h处的非工作状态计算风压，单位为牛顿每平方米')
 mathtemp = r'(N/m^2)'
-width = add_image(mathtemp, 'Nm2')
+add_image(mathtemp, 'Nm2')
 para1.add_run('').add_picture(f'{path}/Nm2.png', height=Mm(font_height))  # Inches(width))
+
+para1 = calc_book.add_paragraph('', style='Normal')
+mathtemp = r'C'
+add_image(mathtemp, 'C')
+para1.add_run('').add_picture(f'{path}/C.png', height=Mm(font_height))
+para1.add_run('——所指构件的空气动力系数，与构件的特征面积A一起使用')
+
+para1 = calc_book.add_paragraph('', style='Normal')
+mathtemp = r'A'
+add_image(mathtemp, 'A')
+para1.add_run('').add_picture(f'{path}/A.png', height=Mm(font_height))
+para1.add_run('——所指构件的特征面积，单位为平方米')
+mathtemp = r'(m^2)'
+add_image(mathtemp, 'm2')
+para1.add_run('').add_picture(f'{path}/m2.png', height=Mm(font_height))  # Inches(width))
+
+calc_book.add_heading('2.3 非工作状态计算风压', level=2)
+calc_book.add_paragraph(f'{tower_model}塔机非工作状态计算风压取深圳防台风技术规程5.1.3中最大风压1870Pa。', style='Normal')
+calc_book.add_paragraph('因此，非工作状态计算风压：', style='Normal')
+mathtemp = r'p_n(h) = 1870 Pa'
+width = add_image(mathtemp, 'pnh1870')
+calc_book.add_paragraph('', style='No Spacing').add_run('').add_picture(f'{path}/pnh1870.png', width=Inches(width))
+
+calc_book.add_heading('2.4 吊臂风载荷计算', level=2)
+
 
 
 
