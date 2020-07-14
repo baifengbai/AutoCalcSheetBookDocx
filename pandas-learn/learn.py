4152651d4764535e60117806b85ad5dc7905cab7
WilliamTsuiChina <342668998@qq.com>
2020-07-14 11:53:48 +0800

    Update learn.py

--- pandas-learn/learn.py
+++ pandas-learn/learn.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import quandl
 
-
-df = quandl.get('WIKI/GOOGL')
-print(df.tail())
-
+# pd.set_option('display.width', 2000)  # 设置字符显示宽度
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)  # 设置显示最大行
+df = pd.read_excel('C1600C390810.xls', 'Sheet1', header=None, skiprows=[0, 1], usecols=[0, 3, 4],
+                   names=['部件名称', '重量', '数量'])
+print(df)
