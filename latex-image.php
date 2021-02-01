cc3c4279a887fc29a2d97dcbbb1dfff40bbf044a
William Tsui <65329282+WilliamTsuiChina@users.noreply.github.com>
2021-02-01 09:36:51 +0800

    dele

--- latex-image.php
+++
@@ -1,8 +0,0 @@
-<?php
-$ch = curl_init();
-$location = curl_escape($ch, $_GET['math']);
-$remoteImage = 'http://chart.googleapis.com/chart?cht=tx&chl='.$location;
-header("Content-type: image/png");
-readfile($remoteImage);
-curl_close($ch);
-?>
